2021aa1f7ccfe6059e7e8ec9f31f4d75f09e8b2b
safv12 <ing.fran12@gmail.com>
2020-06-13 17:39:18 -0500

    Add string guard for null or whitespaces

---
+++ src/KoalaFingers.BasicGuards.Tests/Extensions/GuarExtensionsUnitTests/ThrowIfNullOrWhitespaceTests.cs
@@ -0,0 +1,37 @@
+﻿namespace KoalaFingers.BasicGuards.Tests.Extensions.GuarExtensionsUnitTests
+{
+    using System;
+    using KoalaFingers.BasicGuards.Extensions;
+    using Xunit;
+
+    /// <summary>
+    /// Tests the ThrowIfNullOrWhitespace method in the GuardExtensions file.
+    /// </summary>
+    public partial class GuardExtensionsTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("   ")]
+        public void Given_InvalidStringValue_When_CallIsNullOrWhitespace_Should_ThrowAnArgumentException(string input)
+        {
+            // Act
+            // Assert
+            Assert.Throws<ArgumentException>(() => input.ThrowIfNullOrWhitespace(nameof(input)));
+        }
+
+        [Theory]
+        [InlineData("Sample value")]
+        [InlineData("Hello world")]
+        [InlineData("Input text")]
+        public void Given_ValidStringValue_When_CallIsNullOrWhitespace_Should_ReturnTheInputValue(string input)
+        {
+            // Act
+            var result = input.ThrowIfNullOrWhitespace(nameof(input));
+
+            // Assert
+            Assert.Same(input, result);
+        }
+    }
+}
---
+++ src/KoalaFingers.BasicGuards/Extensions/GuardExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace KoalaFingers.BasicGuards.Extensions
+{
+    /// <summary>
+    /// Guard clauses extensions.
+    /// </summary>
+    public static class GuardExtensions
+    {
+        /// <summary>
+        /// Validates if the provided string value is not null or whitespace.
+        /// </summary>
+        /// <param name="stringValue">String value to validate.</param>
+        /// <param name="propertyName">Name of the tested property.</param>
+        /// <returns>The string value if valid, otherwise throws an argument exception.</returns>
+        public static string ThrowIfNullOrWhitespace(this string stringValue, string propertyName)
+        { 
+            if (string.IsNullOrWhiteSpace(stringValue) || string.IsNullOrEmpty(stringValue))
+            {
+                throw new System.ArgumentException(propertyName);
+            }
+
+            return stringValue;
+        }
+    }
+}
